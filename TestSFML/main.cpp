1c985d83f3b9422c6882e6ee0f6bc1927b2fc3f8
Vincent <vichenzo-thebaud@hotmail.com>
2019-11-07 09:28:07 +0100

    5 squares

--- TestSFML/main.cpp
+++ TestSFML/main.cpp
@@ -8,9 +8,30 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(width, height), "Snake");
 
 	sf::Vector2f rectangleSize(20.f, 20.f);
-	sf::RectangleShape shape(rectangleSize);
-	shape.setFillColor(sf::Color::Green);
-	shape.setPosition((width - rectangleSize.x) / 2.f, (height - rectangleSize.y) / 2.f);
+	float left = 0.f;
+	float top = 0.f;
+	float right = width - rectangleSize.x;
+	float bottom = height - rectangleSize.y;
+
+	sf::RectangleShape rectangle1(rectangleSize);
+	rectangle1.setFillColor(sf::Color::Green);
+	rectangle1.setPosition((width - rectangleSize.x) / 2.f, (height - rectangleSize.y) / 2.f);
+
+	sf::RectangleShape rectangle2(rectangleSize);
+	rectangle2.setFillColor(sf::Color::Red);
+	rectangle2.setPosition(left, top);
+
+	sf::RectangleShape rectangle3(rectangleSize);
+	rectangle3.setFillColor(sf::Color::Blue);
+	rectangle3.setPosition(left, bottom);
+
+	sf::RectangleShape rectangle4(rectangleSize);
+	rectangle4.setFillColor(sf::Color::Magenta);
+	rectangle4.setPosition(right, top);
+
+	sf::RectangleShape rectangle5(rectangleSize);
+	rectangle5.setFillColor(sf::Color::Yellow);
+	rectangle5.setPosition(right, bottom);
 
 	while (window.isOpen())
 	{
@@ -23,7 +44,11 @@ int main()
 
 		window.clear();
 		
-		window.draw(shape);
+		window.draw(rectangle1);
+		window.draw(rectangle2);
+		window.draw(rectangle3);
+		window.draw(rectangle4);
+		window.draw(rectangle5);
 
 		window.display();
 	}
